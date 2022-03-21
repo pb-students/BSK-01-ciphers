037b8949734900607316249d79ef178ff08924ed
luki401 <lukasz.hossa15@gmail.com>
2022-03-21 12:31:30 +0100

    rail fence decrypt done

--- src/composables/useRailFence.ts
+++ src/composables/useRailFence.ts
@@ -9,8 +9,65 @@ const railFenceEncrypt = (message: string, rows: number) => {
 }
 
 const railFenceDecrypt = (message: string, rows: number) => {
+  const res: string[][] = [...Array(rows).keys()].map(() => [])
+  let directory = null;
+  let row = 0;
+  let col = 0;
+  
+  for(let i = 0; i < message.length; i++){
+    if(row === 0){
+      directory = true
+    }
+    if(row === rows - 1){
+      directory = false;
+    }
+    res[row][col] = '*'
+    col = col + 1
 
+    if(directory){
+      row++
+    }
+    else{
+      row--
+    }
+  }
+
+  let index = 0
+  for(let i = 0; i < rows; i++){
+    for(let j = 0; j < message.length; j++){
+      if(res[i][j] === '*' && index < message.length){
+        res[i][j] = message[index];
+        index = index + 1
+      }
+    }
+  }
+  console.log(res)
+  let result = []
+  row = 0
+  col = 0
+  for(let i = 0; i < message.length; i++){
+    if(row === 0){
+      directory = true
+    }
+    if(row === rows - 1){
+      directory = false;
+    }
+
+    if(res[row][col] != '*'){
+      result.push(res[row][col])
+      col++ 
+    }
+
+    if(directory){
+      row++ 
+    }
+    else{
+      row--
+    }
+  }
+  return result.flat().join('');
 }
+
 
 export default () => ({
   railFenceEncrypt,
--- test/railFence.test.ts
+++ test/railFence.test.ts
@@ -1,29 +1,34 @@
 const { railFenceEncrypt, railFenceDecrypt } = useRailFence()
 
-test('encrypt with n = 3', async () => {
+test('encrypt and decrypt with n = 3', async () => {
   const message = 'CRYPTOGRAPHY'
   const n = 3
 
   expect(railFenceEncrypt(message, n)).toEqual('CTARPORPYYGH')
+  expect(railFenceDecrypt(railFenceEncrypt(message, n), n)).toEqual(message)
 })
 
-test('encrypt with n = 4', async () => {
+test('encrypt and decrypt with n = 4', async () => {
   const message = 'CRYPTOGRAPHY'
   const n = 4
 
   expect(railFenceEncrypt(message, n)).toEqual('CGRORYYTAHPP')
+  expect(railFenceDecrypt(railFenceEncrypt(message, n), n)).toEqual(message)
 })
 
-test('encrypt with n = 5', async () => {
+test('encrypt and decrypt with n = 5', async () => {
   const message = 'CRYPTOGRAPHY'
   const n = 5
 
   expect(railFenceEncrypt(message, n)).toEqual('CARRPYGHPOYT')
+  expect(railFenceDecrypt(railFenceEncrypt(message, n), n)).toEqual(message)
+  
 })
 
-test('encrypt with n = 6', async () => {
+test('encrypt and decrypt with n = 6', async () => {
   const message = 'CRYPTOGRAPHY'
   const n = 6
 
   expect(railFenceEncrypt(message, n)).toEqual('CHRPYYAPRTGO')
+  expect(railFenceDecrypt(railFenceEncrypt(message, n), n)).toEqual(message)
 })
