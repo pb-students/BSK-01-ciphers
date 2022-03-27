5d33ba61b18a14c3fee62276fdf2702f706b0ba3
Kasper Seweryn <github@wvffle.net>
2022-03-27 15:46:06 +0200

    feat: add frontend

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImports from 'unplugin-auto-import/vite'
+import Unocss from 'unocss/vite'
+import transformerVariantGroup from '@unocss/transformer-variant-group'
 import { dirResolver, DirResolverHelper } from 'vite-auto-import-resolvers'
 
 // https://vitejs.dev/config/
@@ -8,15 +10,16 @@ export default defineConfig({
   plugins: [
     vue(),
     DirResolverHelper(),
+    Unocss({
+      transformers: [
+        transformerVariantGroup(),
+      ],
+    }),
     AutoImports({
-      imports: ['vue', 'vitest'],
+      imports: ['vue', 'vitest', '@vueuse/core'],
       resolvers: [
         dirResolver()
       ]
     })
-  ],
-
-  test: {
-
-  }
+  ]
 })
