3d9d914b04e3c9615b007f7ca8ee4749fda76176
luki401 <lukasz.hossa15@gmail.com>
2022-03-27 16:56:01 +0200

    add vienegere tests

--- test/vigenere.test.ts
+++ test/vigenere.test.ts
@@ -1,11 +1,34 @@
 const { encrypt, decrypt } = useVigenereCipher()
 const message = 'CRYPTOGRAPHY'
-const key = 'BREAKBREAKBR'
 
-test('encrypt', async () => {
+test('encrypt1', async () => {
+  const key = 'BREAKBREAKBR'
   expect(encrypt(message, key)).toEqual('DICPDPXVAZIP')
 })
 
-test('decrypt', async () => {
+test('decrypt1', async () => {
+  const key = 'BREAKBREAKBR'
   expect(decrypt('DICPDPXVAZIP', key)).toEqual(message)
 })
+
+test('encrypt2', async () => {
+  const key = 'POLITECHNIKA'
+  expect(encrypt(message, key)).toEqual('RFJXMSIYNXRY')
+})
+
+test('decrypt2', async () => {
+  const key = 'POLITECHNIKA'
+  expect(decrypt('RFJXMSIYNXRY', key)).toEqual(message)
+})
+
+test('encrypt3', async () => {
+  const key = 'POLITECHNIK'
+  expect(encrypt(message, key)).toEqual('RFJXMSIYNXRN')
+})
+
+test('decrypt3', async () => {
+  const key = 'POLITECHNIK'
+  expect(decrypt('RFJXMSIYNXRN', key)).toEqual(message)
+})
+
+
